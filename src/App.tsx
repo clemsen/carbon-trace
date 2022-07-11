7c2b01d0cebd7a3d36698bd1db87313b86b4476a
clements <clements@theodo.fr>
2022-07-11 19:33:43 +0200

    add first schema of a company page in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,31 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='Logo'>Logo</div>
+        <div className='header-menu'>Menu</div>
       </header>
+      <div className='company'>
+        <div className='company-header'>
+          <div className='company-logo'>Logo Entreprise </div>
+          <div className='company-name'>Nom Entreprise</div>
+        </div>
+        <div className='company-information'>
+          <div className='carbon-tracability-index'>
+            <div className='scope1'>Scope 1</div>
+            <div className='scope2'>Scope 2</div>
+            <div className='scope-suppliers'>Scope fournisseurs</div>
+          </div>
+          <div className='carbon-euro'>
+            <div className='carbon-euro-tracability'>Carbone par € estimé ISR</div>
+            <div className='carbon-euro-classic'>Carbone par € estimé bilan carbone</div>
+          </div>
+        </div>
+        <div className='alternatives'>
+          <div className='alternative-title'>Alternatives avec meilleurs traçabilités carbones</div>
+          <div className='alternative1'>Marque 1 : ITC (Indice de Traçabilité Carbone) = X1%</div>
+          <div className='alternative2'>Marque 2 : ITC (Indice de Traçabilité Carbone) = X2%</div>
+        </div>
+      </div>
     </div>
   );
 }

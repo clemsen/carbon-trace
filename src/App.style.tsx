e854a1ce349fc505ddbe49ce91cde8b6fdc3a609
clements <clements@theodo.fr>
2022-07-21 19:45:32 +0200

    add nunito sans font to global style

--- src/App.style.tsx
+++ src/App.style.tsx
@@ -1,9 +1,11 @@
 import styled from "styled-components";
+import "@fontsource/nunito-sans";
 
 const StyleApp = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  font-family: "Nunito Sans", sans-serif;
 `;
 
 export default StyleApp;

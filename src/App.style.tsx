0b7f66dcb774f3599013f9af02452822293ab082
clements <clements@theodo.fr>
2022-07-23 16:30:59 +0200

    change header style to adapt to app.tsx align-item

--- src/App.style.tsx
+++ src/App.style.tsx
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 import "@fontsource/nunito-sans";
 
-const StyleApp = styled('div')`
+const StyleApp = styled("div")`
   display: flex;
   flex-direction: column;
+  align-items: center;
   font-family: "Nunito Sans", sans-serif;
 `;
 

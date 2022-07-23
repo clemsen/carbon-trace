2cda00752f43dc368fbea5cd034c23aeaf188e57
thomaseudes <thomeymery@gmail.com>
2022-07-23 13:33:49 +0200

    create homepage and company page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import Company from "./components/Company";
 import Header from "./components/Header";
 import StyleApp from "./App.style";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <StyleApp className="App">
-      <Header />
-      <Company />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<>Insert homepage here</>}></Route>
+          <Route path="/companies" element={<Company />} />
+        </Routes>
+      </BrowserRouter>
     </StyleApp>
   );
 }

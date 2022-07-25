854681d7d144bf330d5e881170e7e23bc75481f6
clements <clements@theodo.fr>
2022-07-25 23:45:46 +0200

    add CompanyItemsTitle

--- src/components/Companies/Companies.component.tsx
+++ src/components/Companies/Companies.component.tsx
@@ -1,15 +1,21 @@
 import StyleCompanies from "./Companies.style";
 import CompanyItem from "./CompanyItem";
+import CompanyItemsTitle from "./CompanyItemsTitle";
+import LateralMenu from "./LateralMenu";
 
 const companyNames: string[] = ["LVMH", "Danone", "Bouygues"];
 
 const Companies = () => {
   return (
     <StyleCompanies className="companies">
-      <h1>Entreprises</h1>
-      {companyNames.map((companyName: string) => (
-        <CompanyItem name={companyName} />
-      ))}
+      <div className="company-items">
+        <h1>Entreprises</h1>
+        <CompanyItemsTitle />
+        {companyNames.map((companyName: string) => (
+          <CompanyItem name={companyName} />
+        ))}
+      </div>
+      <LateralMenu />
     </StyleCompanies>
   );
 };

9971cca9c9a171d77bd2d947b3486f1660293622
clements <clements@theodo.fr>
2022-07-25 23:45:55 +0200

    add LateralMenu

---
+++ src/components/Companies/LateralMenu/LateralMenu.component.tsx
@@ -0,0 +1,7 @@
+import StyleLateralMenu from "./LateralMenu.style";
+
+const LateralMenu = () => {
+  return <StyleLateralMenu>blabla</StyleLateralMenu>;
+};
+
+export default LateralMenu;
---
+++ src/components/Companies/LateralMenu/LateralMenu.style.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const StyleLateralMenu = styled.div`
+  width: 100%;
+  padding-left: 20px;
+  padding-top: 20px;
+  border: solid;
+`;
+
+export default StyleLateralMenu;

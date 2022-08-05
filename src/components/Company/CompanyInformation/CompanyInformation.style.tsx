bbd06be340c4ed16e595deb1e1e239fe20217511
clements <clements@theodo.fr>
2022-08-05 14:57:50 +0200

    add companyInformation component

--- src/components/Company/CompanyInformation/CompanyInformation.style.tsx
+++ src/components/Company/CompanyInformation/CompanyInformation.style.tsx
@@ -2,12 +2,8 @@ import styled from "styled-components";
 
 const StyleCompanyInformation = styled.div`
   width: 100%;
-  margin: 10px;
   display: flex;
   flex-direction: column;
-  > div {
-    margin: 10px 0px 10px 0px;
-  }
 `;
 
 export default StyleCompanyInformation;

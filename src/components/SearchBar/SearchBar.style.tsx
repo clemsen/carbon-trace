3ef2ef9fc2a5a21012de4d0166868db89756436f
thomaseudes <thomeymery@gmail.com>
2022-08-07 15:15:19 +0200

    create search bar component

---
+++ src/components/SearchBar/SearchBar.style.tsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const StyleSearchBar = styled.div`
+  display: flex;
+
+  & .MuiOutlinedInput-root {
+    border-radius: 6px 0px 0px 6px;
+  }
+
+  .lateral-search-button {
+    background-color: #2ace5e;
+    border-radius: 0px 6px 6px 0px;
+    min-width: 30px;
+    width: 50px;
+    box-shadow: none;
+    :hover {
+      background-color: #31ba5c;
+    }
+  }
+`;
+
+export default StyleSearchBar;

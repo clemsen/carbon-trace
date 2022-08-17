3e500ddc7c73a42bb8c28a67ac36deda91791a3c
clements <clements@theodo.fr>
2022-08-17 15:32:10 +0200

    replace modale by mui tooltip

--- src/components/Company/CompanyInformation/InformationItem/InformationItem.component.tsx
+++ src/components/Company/CompanyInformation/InformationItem/InformationItem.component.tsx
@@ -1,6 +1,5 @@
-import React from "react";
+import { Tooltip } from "@mui/material";
 import StyleInformationItem from "./InformationItem.style";
-import InformationModal from "./InformationModal";
 
 interface PropsType {
   itemName: string;
@@ -8,21 +7,13 @@ interface PropsType {
 }
 
 const InformationItem = ({ itemName, itemValue }: PropsType) => {
-  const [showModal, setShowModal] = React.useState(false);
-
   return (
     <StyleInformationItem>
       <div className="info-item-name">
         {itemName}
-        <img
-          src="/images/information.png"
-          alt="information"
-          onMouseEnter={() => setShowModal(true)}
-          onMouseLeave={() => setShowModal(false)}
-        />
-        {showModal && (
-          <InformationModal description="blabla car de la mote qsdlkgjfb qsdmfionqzsd:f misd fmoiqsdbnvkqns viuqz sdq; vioups dcvlqsmdjbncoqsd vjlh bzfemiubc ckjlih" />
-        )}
+        <Tooltip title="sqlfvgn qskldnvqks vmoqsidnv qsdvmoiqs vljsv sjv sif lsdf vldf visdfv sdl vidsfv ">
+          <img src="/images/information.png" alt="information" />
+        </Tooltip>
       </div>
       <div className="info-item-value">{itemValue}</div>
     </StyleInformationItem>

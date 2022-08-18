db846b9d3420785b0262082911028b4e2402854f
clements <clements@theodo.fr>
2022-08-18 20:05:07 +0200

    companies data collection

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,28 @@ import Company from "./components/Company";
 import Footer from "./components/Footer";
 import Homepage from "./components/Homepage";
 
+import { supabase } from "./service/supabaseClient";
+import { useEffect, useState } from "react";
+import { Session } from "@supabase/supabase-js";
+
 function App() {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    setSession(supabase.auth.session());
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
   return (
     <StyleApp className="App">
       <BrowserRouter>
         <Header />
         <Routes>
           <Route path="/" element={<Homepage />}></Route>
-          <Route path="/companies" element={<Companies />} />
+          <Route path="/companies" element={<Companies session={session} />} />
           <Route path="/company/:company" element={<Company />} />
         </Routes>
       </BrowserRouter>
--- src/components/Companies/Companies.component.tsx
+++ src/components/Companies/Companies.component.tsx
@@ -1,17 +1,52 @@
-import { companiesInfo } from "../../constants";
 import StyleCompanies from "./Companies.style";
 import CompanyItem from "./CompanyItem";
 import CompanyItemsTitle from "./CompanyItemsTitle";
 import LateralMenu from "../LateralMenu";
 
-const Companies = () => {
+import { supabase } from "../../service/supabaseClient";
+import { Session } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+
+interface PropsType {
+  session: Session | null;
+}
+
+const Companies = ({ session }: PropsType) => {
+  const [, setLoading] = useState(true);
+  const [companies, setCompanies] = useState<any[] | null>(null);
+
+  useEffect(() => {
+    getCompanies();
+  }, [session]);
+
+  const getCompanies = async () => {
+    try {
+      setLoading(true);
+
+      let { data, error, status } = await supabase.from("Company");
+
+      if (error && status !== 406) {
+        throw error;
+      }
+
+      if (data) {
+        setCompanies(data);
+      }
+    } catch (error) {
+      alert("Error");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <StyleCompanies>
       <div className="company-items">
+        <>{console.log("companies = ", companies)}</>
         <h1>Entreprises</h1>
         <CompanyItemsTitle />
-        {Object.keys(companiesInfo).map((company: string) => (
-          <CompanyItem company={company} />
+        {companies?.map((company: any) => (
+          <CompanyItem company={company.key_name} />
         ))}
       </div>
       <LateralMenu />

e60299de6d1f3d03e3a101ad06f0631e376f5489
thomaseudes <thomeymery@gmail.com>
2022-08-24 18:55:04 +0200

    use react_supabase library

--- src/components/Companies/Companies.component.tsx
+++ src/components/Companies/Companies.component.tsx
@@ -3,39 +3,29 @@ import CompanyItem from "./CompanyItem";
 import CompanyItemsTitle from "./CompanyItemsTitle";
 import LateralMenu from "../LateralMenu";
 
-import { supabase } from "../../service/supabaseClient";
-import { useEffect, useState } from "react";
+import { useSelect } from "react-supabase";
 
 const Companies = () => {
-  const [companies, setCompanies] = useState<any[] | null>(null);
-
-  useEffect(() => {
-    getCompanies();
-  }, []);
-
-  const getCompanies = async () => {
-    try {
-      let { data } = await supabase.from("Company");
-      if (data) {
-        setCompanies(data);
-      }
-    } catch {
-      console.log("Error getting data from supabase");
-    }
-  };
+  const [{ data: companies, error, fetching }] = useSelect("Company");
 
   return (
-    <StyleCompanies>
-      <div className="company-items">
-        <>{console.log("companies = ", companies)}</>
-        <h1>Entreprises</h1>
-        <CompanyItemsTitle />
-        {companies?.map((company: any) => (
-          <CompanyItem company={company.key_name} />
-        ))}
-      </div>
-      <LateralMenu />
-    </StyleCompanies>
+    <>
+      {/* To do : Créer un composant loader et un cas d'erreur */}
+      {fetching && <>Chargement ...</>}
+      {error && <>Une erreur est survenue, veuillez réessayer plus tard</>}
+      {companies && (
+        <StyleCompanies>
+          <div className="company-items">
+            <h1>Entreprises</h1>
+            <CompanyItemsTitle />
+            {companies?.map((company: any) => (
+              <CompanyItem company={company.key_name} />
+            ))}
+          </div>
+          <LateralMenu />
+        </StyleCompanies>
+      )}
+    </>
   );
 };
 

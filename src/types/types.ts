685d2435520bf427a61730f60c72a392a023b190
clements <clements@theodo.fr>
2022-08-25 18:54:51 +0200

    add types file

---
+++ src/types/types.ts
@@ -0,0 +1,9 @@
+export type CompanyCharacteristicType = {
+  id: number;
+  company: string;
+  characteristic: string;
+  value: string;
+  startDate: string;
+  endDate: string;
+  isDisplayed: boolean;
+};

e34f2e3f1849007b0abd0e0fcdcab0ae2f8cf639
clements <clements@theodo.fr>
2022-08-25 18:57:56 +0200

    pass characteristics with props

--- src/components/Companies/CompanyItem/CompanyItem.component.tsx
+++ src/components/Companies/CompanyItem/CompanyItem.component.tsx
@@ -1,52 +1,41 @@
-import { useEffect, useState } from "react";
-import { supabase } from "../../../service/supabaseClient";
+import { CompanyCharacteristicType } from "../../../types/types";
 import StyleCompanyItem from "./CompanyItem.style";
 
 interface PropsType {
-  key_name: string;
+  keyName: string;
   company: string;
+  characteristics: CompanyCharacteristicType[];
 }
 
-const CompanyItem = ({ key_name, company }: PropsType) => {
-  const [companyItc, setCompanyItc] = useState<any | null>(null);
+const CompanyItem = ({ keyName, company, characteristics }: PropsType) => {
+  const getItcValue = (characteristics: CompanyCharacteristicType[]) => {
+    const itcCharacteristic =
+      characteristics.filter(
+        (characteristic) => characteristic.characteristic === "itc"
+      )[0] || null;
 
-  useEffect(() => {
-    getCompanyItc();
-  }, []);
-
-  const getCompanyItc = async () => {
-    try {
-      let { data } = await supabase
-        .from("CompanyCharacteristic")
-        .select()
-        .eq("company", key_name)
-        .eq("characteristic", "itc")
-        .single();
-      if (data) {
-        setCompanyItc(data);
-      }
-    } catch {
-      console.log("Error getting data from supabase");
-    }
+    return itcCharacteristic && itcCharacteristic.value;
   };
 
   return (
     <>
-      {key_name && (
+      {keyName && (
         <StyleCompanyItem
           to={{
-            pathname: "/company/" + key_name,
+            pathname: "/company/" + keyName,
           }}
         >
           <div className="company-item-name-logo">
             <img
               className="company-item-logo"
-              src={"/images/" + key_name + ".png"}
+              src={"/images/" + keyName + ".png"}
               alt={"logo " + company}
             />
             <div className="company-item-name">{company}</div>
           </div>
-          <div className="company-item-information">{companyItc?.value}</div>
+          <div className="company-item-information">
+            {getItcValue(characteristics)}
+          </div>
         </StyleCompanyItem>
       )}
     </>

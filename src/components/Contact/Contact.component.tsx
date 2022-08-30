c7bccd30536502591538ab6eb46bff93d1199ced
clements <clements@theodo.fr>
2022-08-30 07:56:46 +0200

    enable sending email

--- src/components/Contact/Contact.component.tsx
+++ src/components/Contact/Contact.component.tsx
@@ -1,26 +1,44 @@
 import { Button, TextField } from "@mui/material";
+import emailjs from "emailjs-com";
+import { useRef } from "react";
 import LateralMenu from "../LateralMenu";
 import StyleContact from "./Contact.style";
 
 const Contact = () => {
+  const form = useRef<HTMLFormElement>(null);
+
+  const sendEmail = (e: any) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE_ID || "",
+        process.env.REACT_APP_TEMPLATE_ID || "",
+        form.current || "",
+        process.env.REACT_APP_USER_ID
+      )
+      .then(
+        (result) => console.log(result.text),
+        (error) => console.log(error.text)
+      );
+  };
+
   return (
     <StyleContact>
-      <form className="contact-section">
+      <form ref={form} onSubmit={sendEmail} className="contact-section">
         <h2 className="form-label">Votre Email</h2>
         <TextField
-          id="contact-email"
           label="Votre Email"
           variant="outlined"
-          name="contact-email"
+          name="contact_email"
           required
         />
 
         <h2 className="form-label">Votre Message</h2>
         <TextField
-          id="contact-message"
           label="Votre Message"
           variant="outlined"
-          name="contact-message"
+          name="contact_message"
           required
           multiline
         />

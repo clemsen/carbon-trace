513c62408e08fa6497bba6dabf9e3867c6ee0a75
clements <clements@theodo.fr>
2022-09-08 09:30:51 +0200

    add HeaderLinksMobile

--- src/components/Header/Header.component.tsx
+++ src/components/Header/Header.component.tsx
@@ -1,13 +1,15 @@
 import { StyleHeader, StyleHeaderWrapper } from "./Header.style";
 import HeaderLinks from "./HeaderLinks";
 import SiteTitle from "./SiteTitle";
+import HeaderLinksMobile from "./HeaderLinksMobile";
 
 const Header = () => {
   return (
     <StyleHeaderWrapper>
       <StyleHeader className="App-header">
         <SiteTitle />
-        <HeaderLinks />
+        <HeaderLinks className="header-links" />
+        <HeaderLinksMobile className="header-links-mobile" />
       </StyleHeader>
     </StyleHeaderWrapper>
   );
---
+++ src/components/Header/HeaderLinksMobile/HeaderLinksMobile.style.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const StyleHeaderLinksMobile = styled("div")`
+  position: fixed;
+  right: 0;
+  top: 0;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+
+  padding: 10px;
+  background-color: white;
+
+  border-left: solid;
+  border-bottom: solid;
+  border-color: rgb(227, 227, 227);
+`;
+
+export default StyleHeaderLinksMobile;
